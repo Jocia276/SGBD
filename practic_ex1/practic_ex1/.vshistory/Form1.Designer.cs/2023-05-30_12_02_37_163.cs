2879445f4ef201f4315f5004cd5134d65591cd41
Jocia276 <casianaapostol10@gmail.com>
2023-05-30 12:26:06 +0300

    model 1 practic

---
+++ practic_ex1/practic_ex1/.vshistory/Form1.Designer.cs/2023-05-30_12_02_37_163.cs
@@ -0,0 +1,172 @@
+﻿namespace practic_ex1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewChild = new System.Windows.Forms.DataGridView();
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewChild
+            // 
+            this.dataGridViewChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewChild.Location = new System.Drawing.Point(36, 77);
+            this.dataGridViewChild.Name = "dataGridViewChild";
+            this.dataGridViewChild.RowHeadersWidth = 51;
+            this.dataGridViewChild.RowTemplate.Height = 24;
+            this.dataGridViewChild.Size = new System.Drawing.Size(512, 323);
+            this.dataGridViewChild.TabIndex = 0;
+            this.dataGridViewChild.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewChild_CellClick);
+            this.dataGridViewChild.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewChild_CellContentClick);
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(620, 77);
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.RowHeadersWidth = 51;
+            this.dataGridViewParent.RowTemplate.Height = 24;
+            this.dataGridViewParent.Size = new System.Drawing.Size(587, 323);
+            this.dataGridViewParent.TabIndex = 1;
+            this.dataGridViewParent.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewParent_CellClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(977, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 437);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 487);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(236, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(59, 541);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(236, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(437, 446);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(129, 23);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(437, 524);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(129, 23);
+            this.buttonDelete.TabIndex = 7;
+            this.buttonDelete.Text = "delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(437, 605);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(129, 23);
+            this.buttonUpdate.TabIndex = 8;
+            this.buttonUpdate.Text = "update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(59, 594);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(236, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1244, 684);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Controls.Add(this.dataGridViewChild);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewChild;
+        private System.Windows.Forms.DataGridView dataGridViewParent;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ practic_ex1/practic_ex1/.vshistory/Form1.cs/2023-05-30_12_04_40_850.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practic_ex1
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=LAPTOP-1COTFGBR\SQLEXPRESS; Database=Problema1; Integrated Security=true; TrustServerCertificate=true;";
+        DataSet cds = new DataSet();
+        DataSet pds = new DataSet();
+        DataSet childds = new DataSet();
+        SqlDataAdapter parentAdapter = new SqlDataAdapter();
+        SqlDataAdapter childAdapter = new SqlDataAdapter();
+        BindingSource parentBS = new BindingSource();
+        BindingSource childdBS = new BindingSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridViewChild_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    parentAdapter.SelectCommand = new SqlCommand("SELECT * FROM Cofetarii", conn);
+                    pds.Clear(); cds.Clear();
+                    textBox1.DataBindings.Clear(); textBox2.DataBindings.Clear(); textBox3.DataBindings.Clear(); textBox4.DataBindings.Clear();
+                    parentAdapter.Fill(pds);
+                    parentBS.DataSource = pds.Tables[0];
+                    dataGridViewParent.DataSource = parentBS;
+                    dataGridViewChild.DataSource = null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridViewParent_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string id = dataGridViewParent.Rows[e.RowIndex].Cells[0].Value.ToString();
+                childAdapter.SelectCommand = new SqlCommand("SELECT *FROM Briose where cod_cofetarie=" + id + ";", conn);
+                childds.Clear();
+                childAdapter.Fill(childds);
+                childdBS.DataSource = childds.Tables[0];
+                dataGridViewChild.DataSource = childdBS;
+            }
+        }
+
+        private void dataGridViewChild_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                textBox1.DataBindings.Clear(); textBox2.DataBindings.Clear(); textBox3.DataBindings.Clear(); textBox4.DataBindings.Clear();
+
+                textBox1.DataBindings.Add("Text", childdBS, "nume_briosa");
+                textBox2.DataBindings.Add("Text", childdBS, "descriere");
+                textBox3.DataBindings.Add("Text", childdBS, "pret");
+                textBox4.DataBindings.Add("Text", childdBS, "cod_cofetarie");
+
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    int x;
+                    childAdapter.UpdateCommand = new SqlCommand("Update Briose set nume_briosa=@nb, descriere=@d, pret=@p, cod_cofetarie=@c where cod_briosa=@id", conn);
+                    childAdapter.UpdateCommand.Parameters.Add("@nb", SqlDbType.VarChar).Value = textBox1.Text;
+                    childAdapter.UpdateCommand.Parameters.Add("@d", SqlDbType.VarChar).Value = textBox2.Text;
+                    childAdapter.UpdateCommand.Parameters.Add("@p", SqlDbType.Real).Value = textBox3.Text;
+                    childAdapter.UpdateCommand.Parameters.Add("@i", SqlDbType.Int).Value = textBox4.Text;
+                    childAdapter.UpdateCommand.Parameters.Add("@id", SqlDbType.Int).Value = childds.Tables[0].Rows[dataGridViewChild.CurrentCell.RowIndex][0];
+                    conn.Open();
+                    x = childAdapter.UpdateCommand.ExecuteNonQuery();
+                    conn.Close();
+                    if (x >= 1)
+                    {
+                        MessageBox.Show("Briosa a fost actualizata!");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
